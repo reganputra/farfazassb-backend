9f45ea38bd881f943e942f833260dece9e1f2370
bayustwn <khususpby@gmail.com>
2025-06-15 22:49:49 +0700

    refactor: change cors origin to wildcard

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }))
 app.use(helmet());
 app.use(cors({
-    origin : "http://localhost:5173"
+    origin : "*"
 }));
 
 app.use('/api/auth', authRoutes);
